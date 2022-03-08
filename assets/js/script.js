2bd9e88a895155b17c61fd81adf3a8d4b071e2fa
Carrie8348 <maruko_carrie@hotmail.com>
2022-03-08 15:38:17 +0000

    add more js to second html

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,12 @@ function outerFunction() {
 var friends = ["yuki","sigh", "carrie"];
 
 document.getElementById('pal').innerHTML = friends[0];
+
+var cat = {
+    name: "mia",
+    gender: "girl",
+    age: 9,
+    likes: "Salmon"
+}
+
+document.getElementById("pet").innerHTML = cat.name + " is a" + cat.gender + " ," + " she is " + cat.age + " years old" + " and she likes " + cat.likes;
