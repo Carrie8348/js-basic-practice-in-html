2528bdefe1b94e10c23afdee1f123aadac37d10f
Carrie8348 <maruko_carrie@hotmail.com>
2022-03-14 16:18:21 +0000

    add some more js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,28 @@ var cat = {
 }
 
 document.getElementById("pet").innerHTML = cat.name + " is a" + cat.gender + " ," + " she is " + cat.age + " years old" + " and she likes " + cat.likes;
+
+var x = potatoFunction (1, 3);
+document.getElementById("sweetpotato").innerHTML = x;
+
+function potatoFunction (a, b) {
+    return a + b;
+}
+
+function area(a, b) {
+    return a * b;
+}
+document.getElementById("area").innerHTML = "the aera of this square is" + area (3,4)
+
+var SG = {ginger: "Geri", scary: "Mel B", baby: "Emma", sporty: "Mel C", posh: "Victoria"};
+
+document.getElementById("spiceGirls").innerHTML = "She is " + SG.scary;
+
+var person = {
+    name: "Carrie",
+    nationality: "Chinese",
+    placeOfBorn: "Shanghai",
+    gender: "female"
+}
+
+document.getElementById("tieshou").innerHTML = "Tie Shou is from " + person.placeOfBorn;
